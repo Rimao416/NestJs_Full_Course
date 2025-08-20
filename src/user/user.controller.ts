cd72a999e46c480c5182809fe99d4c6d8d773a0a
Rimao416 <kayumbaomari27@gmail.com>
2025-08-20 17:42:40 +0200

    :sparkles: Crud Bookmark

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,13 @@
-import { Controller, Get, Patch, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Patch, UseGuards } from '@nestjs/common';
 import type { User } from 'generated/prisma';
 import { GetUser } from 'src/auth/decorator/get-user.decorator';
 import { JwtGuard } from 'src/auth/guard';
+import { EditUserDto } from './dto';
+import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
+  constructor(private userService: UserService) {}
   @UseGuards(JwtGuard)
   @Get('me')
   getMe(@GetUser() user: User) {
@@ -12,6 +15,6 @@ export class UserController {
     return user;
   }
 
-  @Patch('me')
-  editUser() {}
+  @Patch()
+  editUser(@GetUser('id') userId: number, @Body() dto: EditUserDto) {}
 }
